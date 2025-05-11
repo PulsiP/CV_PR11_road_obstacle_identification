7e4c913fb525d9c629ab2b38b794ecb356012efa
Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>
2025-05-11 10:53:30 +0200

    Re-implement the network interface with new class parameters for greater flexibility
    Signed-off-by: Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>

--- network.py
+++ network.py
@@ -1,10 +1,30 @@
+import abc
 from torch import nn
 from torchvision import models
 from typing import override
-class FCN(nn.Module):
+from torchvision.models.vgg import VGG16_Weights
+
+import os
+os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+
+
+class BaseParams(abc.ABC):
+    def __init__(self):
+        super().__init__()
+
+class FCNParams(BaseParams):
     def __init__(self, num_classes):
+        super().__init__()
+        self.num_classes = num_classes
+
+class FCN(nn.Module):
+    """
+    Simple network for semantic segmentation based on encoder-decoder architecture
+    with VGG16 network-bac
+    """
+    def __init__(self, params:FCNParams):
         super(FCN, self).__init__()
-        backbone = models.vgg16(pretrained=True)
+        backbone = models.vgg16(weights=VGG16_Weights.DEFAULT)
         self.encoder = backbone.features  # Extracts features from VGG16
 
         self.decoder = nn.Sequential(
@@ -21,7 +41,7 @@ class FCN(nn.Module):
             ),  # Output: [batch_size, 64, H/4, W/4]
             nn.ReLU(inplace=True),
             nn.Conv2d(
-                64, num_classes, kernel_size=1
+                64, params.num_classes, kernel_size=1
             ),  # Final layer for segmentation map, reduces channels to num_classes
         )
 
@@ -32,6 +52,4 @@ class FCN(nn.Module):
         segmentation_map = nn.functional.interpolate(
             segmentation_map, size=(96, 256), mode="bilinear", align_corners=False
         )
-
-        print(segmentation_map.shape)
         return segmentation_map
