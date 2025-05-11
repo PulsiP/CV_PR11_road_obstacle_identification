9c3fe35dead701fb67236fa1dd4ab4c54a959d05
Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>
2025-05-11 10:53:59 +0200

    Fixed serious bugs and added new features
    - Fixed serious bug on segmentation mask pre-processing (zeroing useful values).
    - Provided naive implementation for mask processing (works only with 256 classes?).
    - Added code to save the best model of the network
    - Added code to load the best model
    
    Signed-off-by: Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 # log tools
+from typing import Any
 import pandas as pd
 import torch
+import numpy as np
+import os
 from pathlib import Path
 from torch.utils.data import Dataset
 from torch import nn
@@ -8,9 +11,12 @@ from torch.optim import Optimizer
 from torch.utils.data import DataLoader
 from torchmetrics.segmentation import DiceScore
 from tqdm.auto import tqdm
-
 from data import CSDataset
-from network import FCN
+from network import FCN, FCNParams
+from utils import ToMasck, show_tensor
+
+
+from matplotlib import pyplot as plt
 
 
 class TrainNetwork:
@@ -20,13 +26,121 @@ class TrainNetwork:
     configurati
     """
 
-    def __init__(self, model: nn.Module, loss: nn.Module, optimizer: Optimizer) -> None:
+    def __init__(self, hp: dict[str, Any], model: nn.Module) -> None:
         """
         Inizializza il trainer
         """
-        self._model = model
-        self._loss = loss
-        self._optimizer = optimizer
+        self._model: nn.Module = model
+        self._loss: nn.Module = hp["loss"]
+        self._optimizer: Optimizer = hp["optimizer"]
+
+    def __train(
+        self, dataloader_train, model, epoch, loss_fn, device, log_train, train_size
+    ):
+        ##################
+        # Training Phase #
+        ##################
+
+        epoch_loss = 0
+        loss = 0
+        batch = 0
+        model.train()
+        bar = tqdm(
+            desc="training", total=train_size  # type: ignore
+        )  # uses len(dataset) instead of dataset.size
+
+        bar.set_postfix(
+            {
+                "batch": 0,
+                "loss": 0,
+            }
+        )
+
+        for x, y, _ in dataloader_train:
+            bar.set_description(f"training epoch: {epoch}", refresh=True)
+
+            x = x.to(device)
+            y = y.squeeze(1).to(device)
+
+            batch += 1
+            batch_len = len(y)
+
+            self._optimizer.zero_grad()
+            y_pred = model(x)
+
+            loss = loss_fn(y_pred, y)
+            loss_v = loss.item()
+            epoch_loss += loss_v
+            loss.backward()
+
+            self._optimizer.step()
+
+            bar.set_postfix(
+                {
+                    "batch": batch,
+                    "loss": epoch_loss / batch,
+                }
+            )
+            bar.update(batch_len)
+
+        bar.close()
+        log_train["epoch"].append(epoch)
+        log_train["loss"].append(epoch_loss / batch)
+        return log_train
+
+    def __eval(
+        self, dataloader_val, model, epoch, loss_fn, device, log_eval, valid_size
+    ):
+        ####################
+        # Validation Phase #
+        ####################
+        model.eval()
+        epoch_loss = 0
+        loss = 0
+        batch = 0
+
+        bar = tqdm(
+            desc="validation", total=valid_size  # type: ignore
+        )  # uses len(dataset) instead of dataset.size
+
+        bar.set_postfix(
+            {
+                "batch": 0,
+                "loss": 0,
+            }
+        )
+
+        for x, y, _ in dataloader_val:
+
+            with torch.no_grad():
+                bar.set_description(f"validation epoch: {epoch}", refresh=True)
+
+                x = x.to(device)
+                y = y.squeeze(1).to(device)
+
+                batch += 1
+                batch_len = len(y)
+
+                # print(f"label shape {y.shape}")
+                y_pred = model(x)
+
+                loss = loss_fn(y_pred, y)
+                loss_v = loss.item()
+                epoch_loss += loss_v
+
+                bar.set_postfix(
+                    {
+                        "batch": batch,
+                        "loss": epoch_loss / batch,
+                    }
+                )
+                bar.update(batch_len)
+
+        bar.close()
+        log_eval["epoch"].append(epoch)
+        log_eval["loss"].append(epoch_loss / batch)
+
+        return log_eval
 
     def train(
         self,
@@ -35,7 +149,9 @@ class TrainNetwork:
         log_dir: Path | str,
         epochs: int = 1,
         batch_size: int = 1,
-    ):
+        objective: str = "loss",
+        massimize: bool = False,
+    ) -> tuple[nn.Module, dict[str, list[float]], dict[str, list[float]]]:
         """
         Train method, effettua il traning della rete specificata utilizzando come datasets di train e validazione quelli specificati.
         La funzione supporta un semplice metodo automatico di loging
@@ -43,7 +159,14 @@ class TrainNetwork:
 
         """
         device = "cuda" if torch.cuda.is_available() else "cpu"
+        old_weights = ""
         log_dir = Path(log_dir)
+        if not log_dir.exists():
+            os.mkdir(log_dir)
+        else:
+            for f in log_dir.glob("*"):
+                os.remove(f)
+
         dataloader_train = DataLoader(train_set, batch_size=batch_size, shuffle=True)
         dataloader_val = DataLoader(
             validation_set, batch_size=batch_size, shuffle=False
@@ -62,86 +185,51 @@ class TrainNetwork:
         loss_fn = self._loss.to(device)
 
         for epoch in range(1, epochs + 1):
-
-            ##################
-            # Training Phase #
-            ##################
-
-            epoch_loss = 0
-            loss = 0
-            batch = 0
-
-            bar = tqdm(
-                desc="training", total=len(train_set)  # type: ignore
-            )  # uses len(dataset) instead of dataset.size
-
-            bar.set_postfix(
-                {
-                    "batch": batch,
-                    "loss": loss,
-                }
+            self.__train(
+                dataloader_train,
+                model,
+                epoch,
+                loss_fn,
+                device,
+                log_train,
+                len(train_set),
+            )
+            self.__eval(
+                dataloader_val,
+                model,
+                epoch,
+                loss_fn,
+                device,
+                log_eval,
+                len(validation_set),
             )
 
-            for x, y, _ in dataloader_train:
-                bar.set_description(f"training epoch: {epoch}", refresh=True)
+            # save best weigths in agreement with objective value
+            if len(log_eval["epoch"]) < 2 or (
+                (massimize and log_eval[objective][-1] > log_eval[objective][-2])         # if objective will be massimize(>) get the best weights that massimize it
+                or (not massimize and log_eval[objective][-1] < log_eval[objective][-2])  # if objective will be minimize(<) get  save the best weigths  that minimize it
+            ):
+                if old_weights: # old only the best weigths
+                    os.remove(old_weights)
 
-                x = x.to(device)
-                y = y.to(device)
-                batch += 1
-                batch_len = len(y)
+                torch.save(
+                    self._model.state_dict(),
+                    log_dir.joinpath(f"best_weights_epoch_{epoch}.pth"),
+                )
+                old_weights = log_dir.joinpath(f"best_weights_epoch_{epoch}.pth")
 
-                self._optimizer.zero_grad()
-                y_pred = model(x)
-                loss = loss_fn(y, y_pred)
-                epoch_loss += loss.item()
-                loss.backward()
+            # Qalitative learning rapresentation
+            model.eval()
+            with torch.no_grad():
 
-                self._optimizer.step()
-                bar.update(batch_len)
+                x, y, _ = validation_set[0]
+                x = x.unsqueeze(0).to(device)
 
-            log_train["epoch"].append(epoch)
-            log_train["loss"].append(epoch_loss / batch)
+                y_pred = model(x).argmax(dim=1).cpu().numpy().squeeze(0)
+                y = y.cpu().squeeze(0).numpy()
 
-            ####################
-            # Validation Phase #
-            ####################
-
-            epoch_loss = 0
-            loss = 0
-            batch = 0
-
-            bar.close()
-
-            bar = tqdm(
-                desc="validation", total=len(validation_set)  # type: ignore
-            )  # uses len(dataset) instead of dataset.size
-
-            bar.set_postfix(
-                {
-                    "batch": batch,
-                    "loss": loss,
-                }
-            )
-
-            for x, y, _ in dataloader_val:
-
-                with torch.no_grad():
-                    bar.set_description(f"validation epoch: {epoch}", refresh=True)
-
-                    x = x.to(device)
-                    y = y.to(device)
-                    batch += 1
-                    batch_len = len(y)
-
-                    y_pred = model(x)
-                    loss = loss_fn(y, y_pred)
-                    epoch_loss += loss.item()
-
-                    bar.update(batch_len)
-
-            log_eval["epoch"].append(epoch)
-            log_eval["loss"].append(epoch_loss / batch)
-            bar.close()
+                result = np.hstack((y, y_pred))
+                plt.imsave(f"{log_dir}/result_epoch_{epoch}.png", result, cmap="gray")
 
         ###########
         # Logging #
@@ -154,15 +242,38 @@ class TrainNetwork:
             log_dir.joinpath("train.csv"), sep=",", index=False
         )
 
+        # finaly load the best weights
+        best_weigths = torch.load(old_weights, weights_only=True)
+        self._model.load_state_dict(best_weigths)
+        return self._model, log_train, log_eval
+
 
 if __name__ == "__main__":
-    from torchvision.transforms.v2 import Compose, ToTensor
+    from torchvision.transforms import v2
 
-    data_train = CSDataset("Dataset/train", Compose([ToTensor()]))
-    data_test = CSDataset("Dataset/val")
-    model = FCN(4)
-    loss = nn.CrossEntropyLoss()
-    optim = torch.optim.Adam(model.parameters(), lr=0.001)
+    # TODO: add normalization for input images
+    data_train = CSDataset(
+        "Dataset/train",
+        transform_x=v2.Compose(
+            [v2.ToImage(), v2.ToDtype(dtype=torch.float32, scale=True)]
+        ),
+        transform_y=v2.Compose([ToMasck(0, 255)]),
+    )
 
-    trainer = TrainNetwork(model, loss, optim)
-    trainer.train(data_train, data_test, ".", epochs=10, batch_size=8)
+    data_test = CSDataset(
+        "Dataset/val",
+        transform_x=v2.Compose(
+            [v2.ToImage(), v2.ToDtype(dtype=torch.float32, scale=True)]
+        ),
+        transform_y=v2.Compose([ToMasck(0, 255)]),
+    )
+    param = FCNParams(256)
+    model = FCN(param)
+
+    hyper_parameters = {
+        "loss": nn.CrossEntropyLoss(),
+        "optimizer": torch.optim.Adamax(model.parameters()),
+    }
+
+    trainer = TrainNetwork(hyper_parameters, model)
+    m, _, _ = trainer.train(data_train, data_test, "./FCN", epochs=10, batch_size=64)
--- utils.py
+++ utils.py
@@ -1,11 +1,20 @@
 """
 Utility module
 """
+
+import torch
+import cv2 as cv
+import matplotlib.pyplot as plt
 from torch import nn
+from torchvision.transforms.v2 import Grayscale
 from typing import override
-class ImgToClass(nn.Module):
-    def __init__(self) -> None:
-        super(ImgToClass, self).__init__()
+
+
+class ToMasck(nn.Module):
+    def __init__(self, low: int, high: int) -> None:
+        super(ToMasck, self).__init__()
+        self.low = low
+        self.high = high
 
     @override
     def forward(self, x):
@@ -15,4 +24,15 @@ class ImgToClass(nn.Module):
         """
         # TODO: Il metodo prende in input un tensore e/o un'immagine RGB e viene convertita in una maschera numerica (un canale)
         #       di valori per le varie classi di segmento
-        pass 
+        x = cv.cvtColor(x, code=cv.COLOR_RGB2GRAY)
+        x = torch.as_tensor(x, dtype=torch.long)
+        x = torch.clamp(x, min=self.low, max=self.high)
+
+        return x
+
+
+def show_tensor(tensor: torch.Tensor, cmap: str = "gray") -> None:
+    """ """
+    t = tensor.numpy(force=True).transpose((1, 2, 0))
+    plt.imshow(t, cmap=cmap)
+    plt.show()
