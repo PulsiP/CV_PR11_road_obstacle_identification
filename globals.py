12db9b0cbc6d53bf759206bcfdaf319ccf0b83a0
Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>
2025-05-12 14:41:20 +0200

    Add RGB to Class Map
    Signed-off-by: Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>

---
+++ globals.py
@@ -0,0 +1,36 @@
+CITYSCAPES_RGB = {
+    (  0,  0,  0): 0,   # unlabeled
+    ( 70, 70, 70): 1,   # building
+    (190,153,153): 2,   # fence
+    (250,170, 30): 3,   # traffic light
+    (220,220,  0): 4,   # traffic sign
+    (107,142, 35): 5,   # vegetation
+    (152,251,152): 6,   # terrain
+    ( 70,130,180): 7,   # sky
+    (220, 20, 60): 8,   # person
+    (255,  0,  0): 9,   # rider
+    (  0,  0,142):10,   # car
+    (  0,  0, 70):11,   # truck
+    (  0, 60,100):12,   # bus
+    (  0, 80,100):13,   # train
+    (  0,  0,230):14,   # motorcycle
+    (119, 11, 32):15,   # bicycle
+    (128, 64,128):16,   # road
+    (244, 35,232):17,   # sidewalk
+    (153,153,153):18,   # pole
+    (102,102,156):19,   # wall
+    ( 60, 20,220):20,   # dynamic
+    (  0,  0,  0):21,   # ego vehicle
+    (  0,  0,  0):22,   # rectification border
+    (  0,  0,  0):23,   # out of roi
+    (  0,  0,  0):24,   # static
+    ( 81,  0, 81):25,   # ground
+    (230,150,140):26,   # guard rail
+    (232, 35,244):27,   # obstacle
+    ( 70, 70, 70):28,   # parking
+    (102,102,156):29,   # rail track
+    (190,153,153):30,   # service lane
+    (153,153,153):31,   # lane marking
+    (250,170, 30):32,   # license plate
+    (  0,  0,  0):33    # fallback/unknown
+}
