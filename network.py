+ Fixed bugs in metrics calculation with torchmetrics (IoU and DiceScore are now correctly calculated)
+ Eliminated redundant code for object layer processing during evaluation
+ Implemented new evaluation method on obstacles

+ Unresolved issues: AUROC and mAP metrics calculation not completely correct, it is necessary to test the evaluation set in cases without targets)

Signed-off-by: Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>

network.py
@@ -1,12 +1,10 @@
 import abc
-import segmentation_models_pytorch as smp
+import torch
 import torch.nn.functional as F
 from torch import nn
 from torchvision import models
 from typing import override
 from torchvision.models.vgg import VGG19_Weights
-from torchvision.models.resnet import ResNet50_Weights
-from torchvision.models.segmentation.deeplabv3 import DeepLabHead
 
 class BaseParams(abc.ABC):
     def __init__(self):
@@ -54,75 +52,21 @@ class FCN(nn.Module):
         )
         return segmentation_map
 
-
-
-class RNParams(BaseParams):
-    def __init__(self, num_classes):
-        super().__init__()
-        self.num_classes = num_classes
-
-class UnetPlusPlus(nn.Module):
-    def __init__(self, output_classes):
-        super(UnetPlusPlus, self).__init__()
-        self.backbone = smp.UnetPlusPlus(classes=output_classes)
+class BoundaryAwareBCE(nn.Module):
+    def __init__(self, lambda_w=3.0):
+        super(BoundaryAwareBCE, self).__init__()
+        self.lambda_w = lambda_w
     
-    @override
-    def forward(self, x):
-        x = self.backbone(x)
-        x = F.softmax(x, dim=1)
-        return x
+    def forward(self, pred, target, b_mask):
 
-class ResNetSegmentation(nn.Module):
-    """
-    Simple network for semantic segmentation based on encoder-decoder architecture
-    with VGG16 network-bac
-    """
-    def __init__(self, params:RNParams):
-        super(ResNetSegmentation, self).__init__()
+        pred = F.sigmoid(pred)
+        BCE_loss = F.binary_cross_entropy(pred, target, reduction='none')
+        BCE_loss = BCE_loss.mean()
 
-        backbone = models.resnet50(weights=ResNet50_Weights.DEFAULT)
-        modules = list(backbone.children())[:-1]
-        self.encoder = nn.Sequential(*modules) 
-
-        self.head = DeepLabHead(2048, num_classes=512)
-        self.sigmoid = nn.Sigmoid()
-
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(
-                2048, 256, kernel_size=3, stride=2, padding=1, output_padding=1
-            ),  # Output: [batch_size, 256, H/16, W/16]
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(
-                256, 128, kernel_size=3, stride=2, padding=1, output_padding=1
-            ),  # Output: [batch_size, 128, H/8, W/8]
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(
-                128, 64, kernel_size=3, stride=2, padding=1, output_padding=1
-            ),  # Output: [batch_size, 64, H/4, W/4]
-            nn.ReLU(inplace=True),
-            nn.Conv2d(
-                64, params.num_classes, kernel_size=1
-            ),  # Final layer for segmentation map, reduces channels to num_classes
-        )
-
-    @override
-    def forward(self, x):
-        out = self.encoder(x)
+        mask_ones = torch.sum(b_mask == 1)
+        b = (BCE_loss*b_mask).sum()
+        boundary_aware = (self.lambda_w/mask_ones)*b
         
-        #print(out.shape)
-        #out = self.head(out)
-        #print(out.shape)
-        out = self.sigmoid(out)
-        out = self.decoder(out)
+        return BCE_loss + boundary_aware
 
-        #print(segmentation_map.shape)
 
-        segmentation_map = nn.functional.interpolate(
-            out, size=(96, 256), mode="bilinear", align_corners=False
-        )
-
-        #print(segmentation_map.shape)
-
-        segmentation_map = self.sigmoid(segmentation_map)
-        
-        return {"out": segmentation_map}

train.py
@@ -7,15 +7,16 @@ from torch.optim.lr_scheduler import StepLR
 from torchvision.transforms import v2
 from data import CSDataset
 from globals import *
-from network import *
-from utils import ToBMask, ToMask, TrainNetwork,miscoverage_loss, plot_report, BoundaryAwareBCE
-
-import torch.nn.functional as F
+from network import BoundaryAwareBCE, FCN, FCNParams
+from utils import ToBMask, ToMask, TrainNetwork, miscoverage_loss, plot_report
+from evaluation import ObstacleBenchmark
 
 parser = argparse.ArgumentParser(description="CLI")
 
 # 2. Aggiungi i parametri
-parser.add_argument("--epochs", type=int, default=10, help="Numero di epoche")
+parser.add_argument(
+    "--epochs", type=int, default=10, help="Numero di epoche"
+)
 parser.add_argument(
     "--model", type=str, required=True, help="Nome del modello da usare"
 )
@@ -24,9 +25,13 @@ parser.add_argument(
 )
 parser.add_argument("--dataset", type=str, required=True, help="Dataset")
 # 3. Leggi i parametri dalla linea di comando
-args = parser.parse_args()
 
+
+parser.add_argument(
+    "--benchmark", type=str, required=False, help="Test the model on a real scenario", default=""
+)
 # 4. Usa i parametri
+args = parser.parse_args()
 DEVICE = ('cuda' if torch.cuda.is_available() else 'cpu')
 match args.dataset:
 
@@ -164,8 +169,35 @@ m, _, _ = trainer.train(
     batch_size=16,
     map_cls_to_color=MAP_LABEL2COLOR,
 )
+
 plot_report(
     log_file_train=args.log_dir + "/train.csv",
     log_file_valid=args.log_dir + "/eval.csv",
     out_dir=args.log_dir,
 )
+
+
+match args.benchmark:
+    case "Obstacles":
+        benchmark = ObstacleBenchmark(network=m, log_dir=args.log_dir)
+        ENCODE = "one-hot"
+        DATASET_NAME = "LAF192x512"
+        MAP_COLOR2LABEL = LAF_COLOR2LABEL
+        MAP_LABEL2COLOR = LAF_LABEL2COLOR
+        FILL = 0
+        MASK_FN = ToBMask(MAP_COLOR2LABEL, fill=FILL, add_map=LAF_PLUS)
+        NUM_CLS = len(MAP_LABEL2COLOR) + 1
+
+        data_test = CSDataset(
+            f"{DATASET_NAME}/val",
+            transform_x=v2.Compose([v2.ToImage(), v2.ToDtype(dtype=torch.float32, scale=True)]),
+            transform_y=v2.Compose([MASK_FN]),
+
+        )
+        
+        benchmark.run_benchmark(data_test, hyper_parameters["loss"], keep_index=list(LAF_LABEL2COLOR.keys()), format="one-hot", map_cls_to_color=MAP_LABEL2COLOR, device=DEVICE, repeat_for=3)
+    case _:
+        print("--benchmark not found--")
+
+print("--End Script--")
+